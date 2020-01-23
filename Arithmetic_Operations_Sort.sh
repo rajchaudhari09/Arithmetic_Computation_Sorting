7daca127984e913ccdaffe5533b44eed502f107a
Vishal Rajput <vishalpatil01@hotmail.com>
2020-01-23 08:49:45 +0530

    ADDED : Arithmetic Operation 3

--- Arithmetic_Operations_Sort.sh
+++ Arithmetic_Operations_Sort.sh
@@ -7,6 +7,10 @@ echo "a=$a b=$b c=$c"
 #!Calcuting Results Of Arithmetic Operation
 operation1=$((a + b * c))
 operation2=$((a * b + c))
+#! Used scale=2 for display .2 floating value
+operation3=$(echo "scale=2; $c + $a / $b" | bc )
+
 #!Printing Operation and Result
 echo "$a + $b * $c = $operation1"
 echo "$a * $b + $c = $operation2"
+echo "$c + $a / $b = $operation3"
